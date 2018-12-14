ba3f4364ef1a1c55b3ba1ab60574728287d58e4c
Morgane DELMAS <mdelmas93@gmail.com>
2018-12-14 19:48:07 +0100

    clean day7 part1

--- day7/part1.py
+++ day7/part1.py
@@ -1,7 +1,5 @@
 import re
-import numpy as np
 import string
-import operator
 from collections import deque
 
 
@@ -17,7 +15,6 @@ def parse_data() :
 
 def find_starting_step(steps) :
     second_steps = [item for i in steps for item in steps[i]]
-    # print(sorted(set(second_steps)))
     queue = deque()
     for c in string.ascii_uppercase :
         if c not in second_steps :
@@ -28,10 +25,6 @@ steps = parse_data()
 queue = find_starting_step(steps)
 result = ""
 while len(queue) > 0 :
-    print(queue)
-    print(result)
-    print(steps)
-    print()
     curr = queue.popleft()
     while curr in [item for i in steps for item in steps[i]] :
         queue.append(curr)
@@ -46,30 +39,3 @@ while len(queue) > 0 :
     result += curr
 
 print(result)
-
-#
-# print(start)
-# print(steps)
-# queue = deque([start])
-# result = ""
-# print(start)
-# while len(queue) > 0 :
-#   c = queue.popleft()
-#   print(c)
-#
-#   for step in steps :
-#     if step[1] == c and step[0] not in result :
-#       queue.append(c)
-#       c = 0
-#       break
-#
-#   if c :
-#     for step in steps :
-#       if step[0] == c and step[1] not in queue :
-#         queue.append(step[1])
-#
-#     queue = deque(sorted(queue))
-#     print(queue)
-#     result += c
-#
-# print('resultt : ' + result)
